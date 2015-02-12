5b18d3cb5951d14521e5105df29042cfa74534da
Victor Wang <wjp2013@gmail.com>
2015-02-12 22:20:11 +0800

    add todo unit test

--- app/models/todo.rb
+++ app/models/todo.rb
@@ -10,7 +10,7 @@ class Todo < ActiveRecord::Base
   validates :content, presence: true, length: { maximum: 1000 }
   # callbacks .................................................................
   after_create :touch_created_event
-  after_destroy :touch_destroyed_event
+  before_destroy :touch_destroyed_event
   after_save :touch_finished_event
   after_save :touch_assign_user_event
   after_save :touch_change_user_event
@@ -20,6 +20,13 @@ class Todo < ActiveRecord::Base
   attr_accessor :handler_id
   # class methods .............................................................
   # public instance methods ...................................................
+  def finished!
+    update_attribute(:finished, true)
+  end
+
+  def assign_to(user_id)
+    update_attribute(:user_id, user_id)
+  end
   # protected instance methods ................................................
   # private instance methods ..................................................
   # private
@@ -34,23 +41,23 @@ class Todo < ActiveRecord::Base
   end
 
   def touch_finished_event
-    touch_event(handler_id, :todo_finished) if finished.changed?
+    touch_event(handler_id, :todo_finished) if finished_changed?
   end
 
   def touch_changed_expiry_on_event
-    if expiry_on.changed?
+    if expiry_on_changed?
       touch_event(handler_id, :todo_changed_expiry_on, { extra_1: expiry_on_was, extra_2: expiry_on })
     end
   end
 
   def touch_assign_user_event
-    if user_id.changed? && user_id_was.blank?
+    if user_id_changed? && user_id_was.blank?
       touch_event(handler_id, :todo_assign_user, { extra_1: user_id_was, extra_2: user_id })
     end
   end
 
   def touch_change_user_event
-    if user_id.changed? && user_id_was.present?
+    if user_id_changed? && user_id_was.present?
       touch_event(handler_id, :todo_change_user, { extra_1: user_id_was, extra_2: user_id })
     end
   end
--- test/models/todo_test.rb
+++ test/models/todo_test.rb
@@ -1,14 +1,75 @@
 require 'test_helper'
 
 class TodoTest < ActiveSupport::TestCase
+  def todo
+    todo = todos(:one)
+  end
+
   def user
     @user ||= users(:one)
   end
-  # test "the truth" do
-  #   assert true
-  # end
-  test "create todo should be touch created event" do
+
+  def member
+    @member ||= users(:two)
+  end
+
+  def event
+    Event.last
+  end
+
+  test 'finished shoulde be make finished to true' do
+    todo.finished!
+
+    assert todo.finished
+  end
+
+  test 'assign user' do
+    todo.assign_to(member.id)
+
+    assert_equal  member.id, todo.user_id
+  end
+
+  test 'create should be touch created event' do
     todo = Todo.create(content: 'a new todo', handler_id: user.id)
-    assert_equal 1, Event.size
+
+    assert_equal 'todo_created', event.action
+    assert_equal user.id, event.user_id
+  end
+
+  test 'destroy shoudle be touch destroyed event' do
+    todo.handler_id = user.id
+    todo.destroy
+
+    assert_equal 'todo_destroyed', event.action
+    assert_equal user.id, event.user_id
+  end
+
+  test 'finish shoudle be touch finished event' do
+    todo.handler_id = user.id
+    todo.finished!
+
+    assert_equal 'todo_finished', event.action
+    assert_equal user.id, event.user_id
+  end
+
+  test 'assign user from nil to member shoudle be touch assign user event' do
+    todo.handler_id = user.id
+    todo.assign_to(member.id)
+
+    assert_equal 'todo_assign_user', event.action
+    assert_equal user.id, event.user_id
+    assert_equal nil, event.extra_1
+    assert_equal member.id.to_s, event.extra_2
+  end
+
+  test 'change assgin user from user to member shoudle be touch change user event' do
+    todo = todos(:two)
+    todo.handler_id = user.id
+    todo.assign_to(member.id)
+
+    assert_equal 'todo_change_user', event.action
+    assert_equal user.id, event.user_id
+    assert_equal user.id.to_s, event.extra_1
+    assert_equal member.id.to_s, event.extra_2
   end
 end
